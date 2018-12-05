6a69e3666543e5b1c9f56af603b827da188d65ef
kaifox <kajetan.fuchsberger@cern.ch>
2018-12-05 17:53:31 +0100

    removed agencies project. Everything is now merged into molr-mole-*!

--- old-sample/src/main/java/cern/molr/sample/mains/SingleNodeMissionDemoServer.java
+++ old-sample/src/main/java/cern/molr/sample/mains/SingleNodeMissionDemoServer.java
@@ -1,15 +1,15 @@
 package cern.molr.sample.mains;
 
 import cern.molr.sample.conf.DemoSingleNodeMissionConfiguration;
-import org.molr.agency.server.conf.LocalMolrConfiguration;
-import org.molr.agency.server.rest.MolrAgencyRestService;
 import org.molr.mole.core.single.conf.SingleNodeMoleConfiguration;
+import org.molr.mole.server.conf.LocalMolrConfiguration;
+import org.molr.mole.server.rest.MolrMoleRestService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({MolrAgencyRestService.class, SingleNodeMoleConfiguration.class, DemoSingleNodeMissionConfiguration.class, LocalMolrConfiguration.class})
+@Import({MolrMoleRestService.class, SingleNodeMoleConfiguration.class, DemoSingleNodeMissionConfiguration.class, LocalMolrConfiguration.class})
 public class SingleNodeMissionDemoServer {
 
     public static void main(String... args) {
