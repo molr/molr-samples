9bfb838aaeb1da5aa6ff55f232ad304aa274fd90
Andrea Calia <andrea.calia.46@gmail.com>
2018-12-06 16:27:44 +0100

    molr v0.0.5

--- old-sample/src/main/java/cern/molr/sample/mains/SingleNodeMissionDemoServer.java
+++ old-sample/src/main/java/cern/molr/sample/mains/SingleNodeMissionDemoServer.java
@@ -1,15 +1,15 @@
 package cern.molr.sample.mains;
 
 import cern.molr.sample.conf.DemoSingleNodeMissionConfiguration;
+import org.molr.mole.core.conf.LocalSuperMoleConfiguration;
 import org.molr.mole.core.single.conf.SingleNodeMoleConfiguration;
-import org.molr.mole.server.conf.LocalMolrConfiguration;
 import org.molr.mole.server.rest.MolrMoleRestService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({MolrMoleRestService.class, SingleNodeMoleConfiguration.class, DemoSingleNodeMissionConfiguration.class, LocalMolrConfiguration.class})
+@Import({MolrMoleRestService.class, SingleNodeMoleConfiguration.class, DemoSingleNodeMissionConfiguration.class, LocalSuperMoleConfiguration.class})
 public class SingleNodeMissionDemoServer {
 
     public static void main(String... args) {
